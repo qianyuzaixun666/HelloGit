b74330a4053671794d40be7c2074d5a3ef0e4f61
qianyuzaixun666 <qianyuzaixun666@gmail.com>
2022-06-20 17:08:09 +0800

    修改功能分支001

--- HelloGit/src/main/java/com/qianyu/zaixun/HelloGit.java
+++ HelloGit/src/main/java/com/qianyu/zaixun/HelloGit.java
@@ -12,5 +12,6 @@ public class HelloGit {
         System.out.println("在线更新");
         System.out.println("======测试git分支创建,修改,合并======");
         System.out.println("创建功能分支001");
+        System.out.println("修改功能分支001 添加输出666");
     }
 }
